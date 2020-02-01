4b1f156ed0201440da16f37d25ad200ae464325d
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-01 05:30:30 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// App.js
+import React, { useState, useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { useOnClickOutside } from './hooks';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import Burger from './components/Burger';
+import Menu from './components/Menu';
+
+function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div>
+          <h1>Hello. This is burger menu tutorial</h1>
+          <img src="https://media.giphy.com/media/xTiTnwj1LUAw0RAfiU/giphy.gif" alt="animated burger" />
+        </div>
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+export default App;
---
+++ src/global.js
@@ -0,0 +1,28 @@
+// global.js
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+}
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    align-items: center;
+    background: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.primaryLight};
+    display: flex;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    height: 100vh;
+    justify-content: center;
+    text-rendering: optimizeLegibility;
+  }
+  `
